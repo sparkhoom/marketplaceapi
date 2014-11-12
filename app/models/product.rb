9cb2cacf1535063d2614742ae80d77b6fd318f33
sparkhoom <176072396@qq.com>
2014-11-12 15:51:49 +0800

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,20 @@ class Product < ActiveRecord::Base
 										presence: true
 
 	belongs_to :user
+
+  scope :filter_by_title, lambda { |keyword|
+    where("lower(title) like?", "%#{keyword.downcase}")
+  }
+
+  scope :above_or_equal_to_price, lambda { |price| 
+    where("price >= ?", price) 
+  }
+
+  scope :below_or_equal_to_price, lambda { |price|
+    where("price <=?", price)
+  }
+
+  scope :recent, -> {
+    order(:updated_at)
+  }
 end
